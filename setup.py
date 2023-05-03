f79d4d1a7833d7833c8c88b377c2cb17841eb02d
somkietacode <108131461+somkietacode@users.noreply.github.com>
2023-05-03 23:05:12 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='model_saver',
+    version='2.4.0',
+    description='A package for saving and loading machine learning models using pickle',
+    author='somkietacode',
+    author_email='s.r.a.ouedraogo@gmail.com',
+    url='https://github.com/yourusername/model_saver',
+    packages=['model_saver'],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
